576e0909c44b8665cb272687741a95057bb7df95
OgiogioVictor <ogiogiovictor@gmail.com>
2022-06-30 03:49:11 -0600

    fixed comment issues

--- src/modules/addhtmlshows.js
+++ src/modules/addhtmlshows.js
@@ -1,5 +1,8 @@
 import { retrieveShow } from './retrieveshow.js';
 import popupForm from './popup.js';
+import retrieveLikes from './retrievelikes.js';
+import BaseApi from './api.js';
+import checkLikes from './checklikes.js';
 
 const truncate = (str, max, suffix) => (str.length < max ? str : `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`);
 
@@ -14,7 +17,35 @@ const displayModal = (j) => {
   ebModal.style.display = 'block';
 };
 
-const addhtmlShows = (id, name, image, summary, likes) => {
+const addhtmlShows = (id, name, image, summary) => {
+  
+  let mLikes = 0;
+  const promiseLikes = retrieveLikes(BaseApi.Likes);
+  promiseLikes.then((like) =>  { 
+    like.forEach((e) => { 
+      if(Number(e.item_id) === id) { 
+       // console.log(e.likes, '-', id)
+       mLikes = e.likes;
+       if (e.item_id != null) {
+        document.getElementById(`mylikes`).innerHTML = `${mLikes} Likes`;
+       // document.getElementById(`mylikes${e.item_id}`).innerHTML = `${mLikes} Likes`;
+      }
+      return true;
+      //  if(mLikes > 0){
+      //   document.getElementById('mylikes').innerHTML = `${mLikes} Likes`;
+      //  }else if(mLikes === '0' || mLikes === 0 || mLikes === ''){
+      //   document.getElementById('mylikes').innerHTML = `0 Likes`;
+      //  }else {
+      //   document.getElementById('mylikes').innerHTML = `0 Likes`;
+      //  }
+      
+      }
+    })
+   });
+  
+
+  
+
   const tx = summary !== null ? truncate(summary, 100, '...') : 'no summary';
   const showContainer = document.querySelector('.show-container');
   const showItem = document.createElement('div');
@@ -23,7 +54,7 @@ const addhtmlShows = (id, name, image, summary, likes) => {
     <img src="${image}" alt="Shows">
     <div class='show-info'>
       <p>${name}</p>
-      <i class="fas fa-heart">  ${likes} likes</i>
+      <i class="fas fa-heart"> <small data-id="${id}" id='mylikes'>0 Likes</small></i>
     </div>
     <div class="summary">${tx}</div>
     <button type="button" class="show_modal"  data-id="${id}">Comment</button>
--- src/modules/checklikes.js
+++ src/modules/checklikes.js
@@ -1,21 +1,13 @@
-const checkLikes = (id, promiseLikes) => {
-    //console.log("inside likes ")
-   console.log(promiseLikes)
-   // console.log(id, promiseLikes)
+const checkLikes = (id, mLikes) => {
     let likes = 0;
-    //promiseLikes.then((like) => {
-        //console.log(like)
-        promiseLikes.forEach((e) => {
+    console.log(mLikes)
+      return  mLikes.forEach((e) => {
         console.log(e)
         if(Number(e.item_id) === id) {
-         //console.log(e.likes, '-', id)
          return e.likes;
         }
-       // return e.likes;
+        return likes;
       });
-     // return likes
-   // });
-
-   // return likes;
+  //  });
   };
   export default checkLikes;
